e550b4b656df929f3cd6d843c45aaaeb8f59e14e
rupanmentor <rupanguvimentor@gmail.com>
2025-12-01 20:10:19 +0530

    login completed without JWT

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -29,6 +29,23 @@ export const registerUser = async (req, res) => {
   }
 };
 
-
 //Login the user || signin user
 
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userDetail = await User.findOne({ email });
+    if (!userDetail) {
+      return res.status(404).json({ message: "Invalid Email, User Not Found" });
+    }
+    const passwordMatch = await bcrypt.compare(password, userDetail.password);
+    if (!passwordMatch) {
+      return res.status(404).json({ message: "Invalid Password" });
+    }
+    return res
+      .status(200)
+      .json({ message: "User Logged In Successfully", data: userDetail });
+  } catch (error) {
+    res.status(500).json({ message: "User Not Logged In Error in Login user" });
+  }
+};
