4b4c16d26aa62512f899efe547a09f3d1565aac0
rupanmentor <rupanguvimentor@gmail.com>
2025-12-01 19:08:00 +0530

    Model completed

---
+++ Models/user.schema.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+  token: {
+    type: String,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
